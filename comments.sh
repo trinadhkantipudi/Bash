554cf9211348cdaf0d994e19b9dad62af0d0cd37
trinadhkantipudi <56853496+trinadhkantipudi@users.noreply.github.com>
2020-06-09 22:24:37 -0700

    Create comments.sh
    commenting multiple lines

---
+++ comments.sh
@@ -0,0 +1,16 @@
+#for single line comment we use #
+# for commenting multiple lines
+
+: '
+for single line comment
+for single line comment
+for single line comment
+for single line comment
+for single line comment
+for single line comment
+for single line comment
+for single line comment
+for single line comment
+for single line comment '
+
+echo "hello world"
